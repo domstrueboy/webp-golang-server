39e2fd01b2aae4efae1d5948468c4e73b665943d
p.bobylev <p.bobylev@info74.ru>
2019-04-29 16:23:55 +0500

    files can save

--- server.go
+++ server.go
@@ -29,38 +29,29 @@ func main() {
 
 func uploadFileHandler() http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Print("1")
 		// validate file size
 		r.Body = http.MaxBytesReader(w, r.Body, maxUploadSize)
 		if err := r.ParseMultipartForm(maxUploadSize); err != nil {
 			renderError(w, "FILE_TOO_BIG", http.StatusBadRequest)
 			return
 		}
-		log.Print("2", r.Body)
 		// parse and validate file and post parameters
-		fileType := r.PostFormValue("type")
+		// fileType := r.PostFormValue("type")
 		file, _, err := r.FormFile("file")
-		log.Print("3", fileType, file, err)
 		if err != nil {
 			renderError(w, "INVALID_FILE", http.StatusBadRequest)
 			return
 		}
-		log.Print("4")
 		defer file.Close()
 		fileBytes, err := ioutil.ReadAll(file)
-		log.Print("5")
 		if err != nil {
 			renderError(w, "INVALID_FILE", http.StatusBadRequest)
 			return
 		}
-
-		log.Print("6")
 
 		// check file type, detectcontenttype only needs the first 512 bytes
 		filetype := http.DetectContentType(fileBytes)
-		log.Print(filetype)
 
-		log.Print("7")
 		switch filetype {
 		case "image/jpeg", "image/jpg":
 		case "image/gif", "image/png":
@@ -70,34 +61,30 @@ func uploadFileHandler() http.HandlerFunc {
 			renderError(w, "INVALID_FILE_TYPE", http.StatusBadRequest)
 			return
 		}
-		log.Print("8")
 		fileName := randToken(12)
-		log.Print(fileName)
 		fileEndings, err := mime.ExtensionsByType(filetype)
-		log.Print(fileEndings)
+
 		if err != nil {
 			renderError(w, "CANT_READ_FILE_TYPE", http.StatusInternalServerError)
 			return
 		}
-		log.Print("9")
+
 		newPath := filepath.Join(uploadPath, fileName+fileEndings[0])
 		fmt.Printf("FileType: %s, File: %s\n", filetype, newPath)
 
 		// write file
-		log.Print("10")
-		log.Print(newPath)
 		newFile, err := os.Create(newPath)
 		if err != nil {
 			renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
 			return
 		}
-		log.Print("11")
+
 		defer newFile.Close() // idempotent, okay to call twice
 		if _, err := newFile.Write(fileBytes); err != nil || newFile.Close() != nil {
 			renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
 			return
 		}
-		log.Print("12")
+
 		w.Write([]byte("SUCCESS"))
 	})
 }
